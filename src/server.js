143e7d88b9b90b77ed886af777a0de506d124779
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-04-19 16:02:16 -0700

    Azure auth testing with easy auth/single tenant/new app registration

--- src/server.js
+++ src/server.js
@@ -14,20 +14,28 @@ const create = async () => {
 
     // server
     const app = express();
-
+    
+    // Trust the Azure proxy, test HTTPS with req.secure
+    app.set('trust proxy', 1)
+    
     // configure nonFeature
     app.use(ignoreFavicon);
 
-    // root route - serve static file
+    // Root route - serve static file
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/client.html'));
     });
-
+    
+    // Authentication Redirect - serve static file
+    app.get('/.auth/login/aad/callback', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/authenticated.html'));
+    });
+    
     // Error handler
     /* eslint-disable no-unused-vars */
     app.use((err, req, res, next) => {
         console.error(err.stack);
-        res.status(500).send('Something broke!');
+        res.status(500).send('Something broke! ' + req.route.path);
     });
     return app;
 };
